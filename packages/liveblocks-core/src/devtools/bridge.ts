4426d0402c56342598f1f64f3f30099c3efb9e08
Vincent Driessen <me@nvie.com>
2022-11-10 11:00:40 +0100

    DO NOT COMMIT! Show message logs for now

--- packages/liveblocks-core/src/devtools/bridge.ts
+++ packages/liveblocks-core/src/devtools/bridge.ts
@@ -35,24 +35,25 @@ export function sendToPanel(
 
   if (!(options?.force || _bridgeActive)) {
     // eslint-disable-next-line rulesdir/console-must-be-fancy
-    // console.log(
-    //   "%c[client → panel] %c%s",
-    //   "color: green",
-    //   "color: gray; font-weight: bold",
-    //   fullMsg.msg,
-    //   "[🚫 NOT sent!]"
-    // );
+    console.log(
+      "%c[client → panel] %c%s",
+      "color: green",
+      "color: green; font-weight: bold",
+      fullMsg.msg,
+      "[🚫 NOT sent!]",
+      fullMsg
+    );
     return;
   }
 
   // eslint-disable-next-line rulesdir/console-must-be-fancy
-  // console.log(
-  //   "%c[client → panel] %c%s",
-  //   "color: green",
-  //   "color: green; font-weight: bold",
-  //   fullMsg.msg,
-  //   fullMsg
-  // );
+  console.log(
+    "%c[client → panel] %c%s",
+    "color: green",
+    "color: green; font-weight: bold",
+    fullMsg.msg,
+    fullMsg
+  );
   window.postMessage(fullMsg, "*");
 }
 
@@ -65,13 +66,13 @@ if (process.env.NODE_ENV !== "production" && typeof window !== "undefined") {
       event.source === window &&
       event.data?.source === "liveblocks-devtools-panel"
     ) {
-      // console.log(
-      //   "%c[client ← panel] %c%s",
-      //   "color: purple",
-      //   "color: purple; font-weight: bold",
-      //   event.data.msg,
-      //   event.data
-      // );
+      console.log(
+        "%c[client ← panel] %c%s",
+        "color: purple",
+        "color: purple; font-weight: bold",
+        event.data.msg,
+        event.data
+      );
       eventSource.notify(event.data);
     } else {
       // Message not for us
